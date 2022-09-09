f678aa51589db08f5fcacae510cbd1a5935d410e
Aaron Jimenez <aajc_96@hotmail.com>
2022-09-08 23:12:27 -0500

    front terminado, comienza lo bueno..

--- card-interactive/src/components/Card.jsx
+++ card-interactive/src/components/Card.jsx
@@ -4,8 +4,10 @@ import CardBack from '../images/bg-card-back.png';
 
 function Card(){
     return(
-        <div>
-            <img src={CardFront} />
+        <div className='relative'>
+            <svg className="absolute inset-2.5"
+            width="84" height="47" fill="none" xmlns="http://www.w3.org/2000/svg"><ellipse cx="23.478" cy="23.5" rx="23.478" ry="23.5" fill="#fff"/><path d="M83.5 23.5c0 5.565-4.507 10.075-10.065 10.075-5.559 0-10.065-4.51-10.065-10.075 0-5.565 4.506-10.075 10.065-10.075 5.558 0 10.065 4.51 10.065 10.075Z" stroke="#fff"/></svg>
+            <img className="card-front" src={CardFront} />
             <img src={CardBack} className='pl-8 pt-5' />
         </div>
     );
--- card-interactive/src/components/Form.jsx
+++ card-interactive/src/components/Form.jsx
@@ -1,26 +1,40 @@
 import React from 'react'
 import '../styles/Form.css'
+import { useState } from 'react';
 
+function Form() {
+    const [input, getInput] = useState('');
 
-function Form({ name }) {
+    const getValue = e => {
+        console.log(e.target.value);
+        getInput(e.target.value);
+    }
+
+    const submitData = () => {
+        console.log('Event submit')
+    }
+    
     return (
-        <div className='flex flex-col space-y-2'>
-            <label>NAME</label>
-            <input type='text' placeholder='Your full name' className='rounded pl-1'></input>
+        <div >
+            <form className='flex flex-col space-y-3' onSubmit={submitData}>
+                <label className='text-sm text-gray-600'>CARDHOLDER NAME</label>
+                <input type='text' name='name' placeholder='e.g Aaron Jimenez' className='rounded pl-1' onChange={getValue}></input>
 
-            <label>NUMBER</label>
-            <input type='text' placeholder='Your full name' className='rounded pl-1'></input>
+                <label className='text-sm text-gray-600'>CARD NUMBER</label>
+                <input type='text'  placeholder='e.g 1234 5678 1234 5678' className='rounded pl-1'  onChange={getValue}></input>
 
-            <div className='flex space-x-8'>
-                <label>EXT. DATE (MM/YY)</label>
-                <label>CVV</label>
-            </div>
-            <div className='flex space-x-3'>
-                <input type='text' placeholder='MM' className='w-20 rounded pl-1'></input>
-                <input type='text' placeholder='YY' className='w-20 rounded pl-1'></input>
-                <input type='text' placeholder='CVV' className='w-20 rounded pl-1'></input>
-            </div>
-            <button type='submit' className='w-50 mt-4 p-1 bg-violet-900 text-white rounded-lg'>SAVE</button>
+                <div className='flex space-x-8'>
+                    <label className='text-sm text-gray-600'>EXT. DATE (MM/YY)</label>
+                    <label className='text-sm text-gray-600'>CVV</label>
+                </div>
+                <div className='flex space-x-3'>
+                    <input type='number' placeholder='MM' className='w-20 rounded pl-1'  onChange={getValue}></input>
+                    <input type='number' placeholder='YY' className='w-20 rounded pl-1'  onChange={getValue}></input>
+                    <input type='number' placeholder='CVV' className='w-20 rounded pl-1'  onChange={getValue}></input>
+                </div>
+                <button type='submit' className='w-50 mt-5 p-1 bg-violet-900 text-white rounded-lg' onClick={submitData}>SAVE</button>
+            </form>
+           
         </div>
     );
 }
