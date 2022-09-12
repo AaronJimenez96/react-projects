7289b9be2f866fde3d156f3e5a9270b4b2591133
Aaron Jimenez <ajimenez@ficachi.com.mx>
2022-09-12 16:50:43 -0500

    validation input

--- card-interactive/src/components/Card.jsx
+++ card-interactive/src/components/Card.jsx
@@ -1,15 +1,30 @@
 import React from "react";
-import CardFront from '../images/bg-card-front.png';
-import CardBack from '../images/bg-card-back.png';
+import CardFront from "../images/bg-card-front.png";
+import CardBack from "../images/bg-card-back.png";
 
-function Card(){
-    return(
-        <div className='relative'>
-            <svg className="absolute inset-2.5"
-            width="84" height="47" fill="none" xmlns="http://www.w3.org/2000/svg"><ellipse cx="23.478" cy="23.5" rx="23.478" ry="23.5" fill="#fff"/><path d="M83.5 23.5c0 5.565-4.507 10.075-10.065 10.075-5.559 0-10.065-4.51-10.065-10.075 0-5.565 4.506-10.075 10.065-10.075 5.558 0 10.065 4.51 10.065 10.075Z" stroke="#fff"/></svg>
+function Card(props) {
+  return (
+    <div className="static">
+      <div className="static grid">
+            <svg
+            className="absolute"
+            width="84"
+            height="47"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <ellipse cx="23.478" cy="23.5" rx="23.478" ry="23.5" fill="#fff" />
+            <path
+                d="M83.5 23.5c0 5.565-4.507 10.075-10.065 10.075-5.559 0-10.065-4.51-10.065-10.075 0-5.565 4.506-10.075 10.065-10.075 5.558 0 10.065 4.51 10.065 10.075Z"
+                stroke="#fff"/>
+            </svg>
+            <label className="absolute">Aaron Jimenez</label>
             <img className="card-front" src={CardFront} />
-            <img src={CardBack} className='pl-8 pt-5' />
-        </div>
-    );
+      </div>
+      <div className="static grid">
+        <img src={CardBack} className="pl-8 pt-5" />
+        <label className="absolute">064</label>
+      </div>
+    </div>
+  );
 }
 export default Card;
--- card-interactive/src/components/Form.jsx
+++ card-interactive/src/components/Form.jsx
@@ -2,37 +2,38 @@ import React from 'react'
 import '../styles/Form.css'
 import { useState } from 'react';
 
-function Form() {
-    const [input, getInput] = useState('');
+function Form(props) {
+    const [name, setMessage] = useState('');
+    const [number, setNumber] = useState('');
 
-    const getValue = e => {
-        console.log(e.target.value);
-        getInput(e.target.value);
-    }
+  const handleName = event => {
+    const result = event.target.value.replace(/[^a-z ]/gi, '');
+    setMessage(result);
+  };
 
-    const submitData = () => {
-        console.log('Event submit')
-    }
-    
+  const handleCardHolder = event => {
+    const result2 =  event.target.valueevent.target.value.replace(/[^0-9]/g, "");
+    setNumber(result2);
+  }
     return (
         <div >
-            <form className='flex flex-col space-y-3' onSubmit={submitData}>
+            <form className='flex flex-col space-y-3'>
                 <label className='text-sm text-gray-600'>CARDHOLDER NAME</label>
-                <input type='text' name='name' placeholder='e.g Aaron Jimenez' className='rounded pl-1' onChange={getValue}></input>
+                <input type='text' name='name' value={name} placeholder='e.g Aaron Jimenez' className='rounded pl-1' onChange={handleName}></input>
 
                 <label className='text-sm text-gray-600'>CARD NUMBER</label>
-                <input type='text'  placeholder='e.g 1234 5678 1234 5678' className='rounded pl-1'  onChange={getValue}></input>
+                <input type='number' name='number' pattern="[0-9]*" value={number} placeholder='e.g 1234 5678 1234 5678' className='rounded pl-1'  onChange={handleCardHolder}></input>
 
                 <div className='flex space-x-8'>
                     <label className='text-sm text-gray-600'>EXT. DATE (MM/YY)</label>
                     <label className='text-sm text-gray-600'>CVV</label>
                 </div>
                 <div className='flex space-x-3'>
-                    <input type='number' placeholder='MM' className='w-20 rounded pl-1'  onChange={getValue}></input>
-                    <input type='number' placeholder='YY' className='w-20 rounded pl-1'  onChange={getValue}></input>
-                    <input type='number' placeholder='CVV' className='w-20 rounded pl-1'  onChange={getValue}></input>
+                    <input type='number' placeholder='MM' className='w-20 rounded pl-1'></input>
+                    <input type='number' placeholder='YY' className='w-20 rounded pl-1'></input>
+                    <input type='number' placeholder='CVV' className='w-20 rounded pl-1'></input>
                 </div>
-                <button type='submit' className='w-50 mt-5 p-1 bg-violet-900 text-white rounded-lg' onClick={submitData}>SAVE</button>
+                <button type='submit' className='w-50 mt-5 p-1 bg-violet-900 text-white rounded-lg'>SAVE</button>
             </form>
            
         </div>
