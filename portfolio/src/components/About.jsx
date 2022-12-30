0c2f815903ee293bf3aa37d4e03e54f0a01fe6ae
Aaron <aajc_96@hotmail.com>
2022-12-29 21:40:35 -0600

    Update About.jsx

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -1,11 +1,78 @@
 import React from "react";
+import Code from "../img/About.png";
 
 function About() {
   return (
-    <section>
-      <h1></h1>
+    <section id="about">
+      <div className="flex  items-center justify-center p-4">
+        <div className="max-w-6xl overflow-hidden rounded-xl bg-white shadow-md duration-200 hover:scale-105 hover:shadow-xl">
+          <div className="sm:flex block space-x-6">
+            <div className="w-full flex justify-center">
+              <img src={Code} alt="Coding" className="h-72 w-auto m-2" />
+            </div>
+            <div className="block">
+              <h1 className="mb-4 text-3xl font-extrabold text-center text-gray-900 dark:text-white md:text-5xl lg:text-4xl">
+                <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+                  Let's work
+                </span>{" "}
+                together.
+              </h1>
+              <p className="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+              I've been working as a <strong className="text-black">front-end developer</strong> for almost 2 years, I
+                like helping to build unique projects and collaborating width
+                diverse teams. I'm flexible and I like to connect to people.
+                <br />
+              </p>
+
+           
+              <p className="underline underline-offset-8 text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+                These are my skills to taking projects to another level
+              </p>
+              <div className="mt-3 flex flex-wrap">
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                >
+                  HTML5
+                </button>
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                >
+                  CSS3
+                </button>
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                >
+                  TAILWIND
+                </button>
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                >
+                  JAVASCRIPT
+                </button>
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                >
+                  REACT JS
+                </button>
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                >
+                  GIT
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
 
 export default About;
+
